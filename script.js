4c7a4ab94fc3e059fc1ae4982ae10233965183ee
ryan1ryanryan <148781776+ryan1ryanryan@users.noreply.github.com>
2024-09-21 20:33:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,30 +3,30 @@ const floatAnimation = () => {
          /*Can Animation*/
         tlCan
             //move top left
-        .to('.can > img', 3, { y:'-=30', x:'+=20',  rotation:'-=5', ease:Power1.easeInOut})
+        .to('.Jarrel > img', 3, { y:'-=30', x:'+=20',  rotation:'-=5', ease:Power1.easeInOut})
         
             //move down right
-        .to('.can > img', 2, { y:'+=30', x:'-=20', rotation:'-=5', ease:Power1.easeInOut})
+        .to('.Jarrel > img', 2, { y:'+=30', x:'-=20', rotation:'-=5', ease:Power1.easeInOut})
         
         
-        .to('.can > img', 3, { y:'-=20',  rotation:'+=5', ease:Power1.easeInOut})
+        .to('.Jarrel > img', 3, { y:'-=20',  rotation:'+=5', ease:Power1.easeInOut})
         
-        .to('.can > img', 3, { y:'+=20',  rotation:'+=5', ease:Power1.easeInOut})
+        .to('.Jarrel > img', 3, { y:'+=20',  rotation:'+=5', ease:Power1.easeInOut})
         
         
-        .to('.can > imgn', 3, { y:'-=50', ease:Power1.easeInOut})
+        .to('.Jarrel > imgn', 3, { y:'-=50', ease:Power1.easeInOut})
            
-        .to('.can > img', 3, { y:'+=50', ease:Power1.easeInOut})
+        .to('.Jarrel > img', 3, { y:'+=50', ease:Power1.easeInOut})
         
         
-        .to('.can > img', 3, { y:'-=30', ease:Power1.easeInOut})
+        .to('.Jarrel > img', 3, { y:'-=30', ease:Power1.easeInOut})
            
-        .to('.can > img', 3, { y:'+=30', ease:Power1.easeInOut})
+        .to('.Jarrel > img', 3, { y:'+=30', ease:Power1.easeInOut})
         
         
-        .to('.can > img', 2, { y:'-=30', ease:Power1.easeInOut})
+        .to('.Jarrel > img', 2, { y:'-=30', ease:Power1.easeInOut})
            
-        .to('.can > img', 2, { y:'+=30', ease:Power1.easeInOut})
+        .to('.Jarrel > img', 2, { y:'+=30', ease:Power1.easeInOut})
   
       TweenLite.to(tlCan, 27, {ease:Power1.easeInOut})
 
