31fff805d79aca4e03c3ef09a7c3204aae965502
ryan1ryanryan <148781776+ryan1ryanryan@users.noreply.github.com>
2024-09-21 20:59:55 +0800

    Update qijun.js

--- qijun.js
+++ qijun.js
@@ -1,1 +1,16 @@
+ const walker = document.getElementById('walker');
+        let position = -100;
+        const windowWidth = window.innerWidth;
+        
+        function moveWalker() {
+            position += 5;
+            walker.style.left = position + 'px';
 
+            if (position > windowWidth) {
+                position = -100;
+            }
+            
+            requestAnimationFrame(moveWalker);
+        }
+
+        moveWalker();
