cf66610175e1dee997747f64a6035d330b90eb65
ryan1ryanryan <148781776+ryan1ryanryan@users.noreply.github.com>
2024-10-04 11:34:55 +0800

    Update qijun.js

--- qijun.js
+++ qijun.js
@@ -4,23 +4,24 @@ let tiltDirection1 = 1;
 const windowWidth = window.innerWidth;
 
 function moveWalker1() {
-    position1 += 5;
-    walker1.style.left = position1 + 'px';
+  position1 += 5;
+  walker1.style.left = position1 + 'px';
 
-    if (position1 > windowWidth) {
-        position1 = -100;
-    }
+  if (position1 > windowWidth) {
+    position1 = -100;
+  }
 
-    requestAnimationFrame(moveWalker1);
+  requestAnimationFrame(moveWalker1);
 }
 
 function toggleTilt1() {
-    walker1.style.transform = `rotate(${tiltDirection1 * 10}deg)`;
-    tiltDirection1 *= -1;
+  walker1.style.transform = `rotate(${tiltDirection1 * 10}deg)`;
+  tiltDirection1 *= -1;
 }
 
+// Keep the setTimeout for Jarrel's delay
 setTimeout(() => {
-    moveWalker1();
+  moveWalker1();
 }, 2000);
 
 setInterval(toggleTilt1, 400);
@@ -30,19 +31,19 @@ let position2 = -100;
 let tiltDirection2 = 1;
 
 function moveWalker2() {
-    position2 += 5;
-    walker2.style.left = position2 + 'px';
+  position2 += 5;
+  walker2.style.left = position2 + 'px';
 
-    if (position2 > windowWidth) {
-        position2 = -100;
-    }
+  if (position2 > windowWidth) {
+    position2 = -100;
+  }
 
-    requestAnimationFrame(moveWalker2);
+  requestAnimationFrame(moveWalker2);
 }
 
 function toggleTilt2() {
-    walker2.style.transform = `rotate(${tiltDirection2 * 10}deg)`;
-    tiltDirection2 *= -1;
+  walker2.style.transform = `rotate(${tiltDirection2 * 10}deg)`;
+  tiltDirection2 *= -1;
 }
 
 moveWalker2();
