76e057940ce9017c891bba503b0a1fe1120c38b5
ryan1ryanryan <148781776+ryan1ryanryan@users.noreply.github.com>
2024-10-04 11:24:10 +0800

    Update qijun.js

--- qijun.js
+++ qijun.js
@@ -1,28 +1,49 @@
-const walker = document.getElementById('qijun');
-let position = -100;
-let tiltDirection = 1;
+const walker1 = document.getElementById('jarrel');
+let position1 = -100;
+let tiltDirection1 = 1;
 const windowWidth = window.innerWidth;
 
-// Function to move the walker image smoothly
-function moveWalker() {
-    position += 5;
-    walker.style.left = position + 'px';
+function moveWalker1() {
+    position1 += 5;
+    walker1.style.left = position1 + 'px';
 
-    if (position > windowWidth) {
-        position = -100;
+    if (position1 > windowWidth) {
+        position1 = -100;
     }
 
-    requestAnimationFrame(moveWalker);
+    requestAnimationFrame(moveWalker1);
 }
 
-// Function to toggle tilt every 1 second
-function toggleTilt() {
-    walker.style.transform = `rotate(${tiltDirection * 10}deg)`;
-    tiltDirection *= -1; // Reverse the tilt direction
+function toggleTilt1() {
+    walker1.style.transform = `rotate(${tiltDirection1 * 10}deg)`;
+    tiltDirection1 *= -1;
 }
 
-// Start the movement
-moveWalker();
+setTimeout(() => {
+    moveWalker1();
+}, 2000);
 
-// Change tilt every 1 second
-setInterval(toggleTilt, 400);
+setInterval(toggleTilt1, 400);
+
+const walker2 = document.getElementById('qijun');
+let position2 = -100;
+let tiltDirection2 = 1;
+
+function moveWalker2() {
+    position2 += 5;
+    walker2.style.left = position2 + 'px';
+
+    if (position2 > windowWidth) {
+        position2 = -100;
+    }
+
+    requestAnimationFrame(moveWalker2);
+}
+
+function toggleTilt2() {
+    walker2.style.transform = `rotate(${tiltDirection2 * 10}deg)`;
+    tiltDirection2 *= -1;
+}
+
+moveWalker2();
+setInterval(toggleTilt2, 400);
